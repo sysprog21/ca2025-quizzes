ff1f33562d318d8aa7f24a1cf1d51c7395fba7e3
jgw0915 <jgw.0915.tpc@gmail.com>
2025-10-10 16:53:41 +0800

    Test/Adding test case for clz.c

--- uf8/clz.c
+++ uf8/clz.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef uint8_t uf8;
 
@@ -14,13 +16,55 @@ static inline unsigned clz(uint32_t x)
         }
         c >>= 1;
     } while (c);
+    // printf(" n = %d , x = %u \n", n, x);
     return n - x;
+}
+
+bool test_clz_0x1()
+{
+    uint32_t x = 0x1;
+    unsigned expected = 31;
+    unsigned result = clz(x);
+    if (result != expected) {
+        printf("clz(0x%X) = %u, expected %u\n", (unsigned)x, result, expected);
+        return false;
+    }
+    return true;
+}
+
+bool test_clz_0x80()
+{
+    uint32_t x = 0x80;
+    unsigned expected = 24;
+    unsigned result = clz(x);
+    if (result != expected) {
+        printf("clz(0x%X) = %u, expected %u\n", (unsigned)x, result, expected);
+        return false;
+    }
+    return true;
+}
+
+bool test_clz_0x00000000()
+{
+    uint32_t x = 0x00000000;
+    unsigned expected = 32;
+    unsigned result = clz(x);
+    if (result != expected) {
+        printf("clz(0x%X) = %u, expected %u\n", (unsigned)x, result, expected);
+        return false;
+    }
+    return true;
 }
 
 
 int main(void)
 {
-    uint32_t x = 0x10;
-    printf("Leading Zero of %d = %d .\n", x, clz(0x10));
+    bool all_passed = false;
+    all_passed |= test_clz_0x1();
+
+
+    if (all_passed) {
+        printf("All Test passed.\n");
+    };
     return 1;
 }
